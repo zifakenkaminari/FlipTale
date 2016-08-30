825cea7bc2fabf7479ed89e17627cd718a4b601d
Sean <b03902126@csie.ntu.edu.tw>
2016-08-30 20:50:47 +0900

    Create Entity: flip feature.

---
+++ Assets/Script/Entity.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+    public Sprite frontSprite;
+    public Sprite backSprite;
+    public bool face;           //not flipped at first
+    public bool isFlipping;
+    public float flipTime;
+    public float flipPeriod;
+    protected Rigidbody rigidbody;
+
+    public Vector3 saveVelocity;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        face = true;
+        isFlipping = false;
+        flipTime = -10000f;
+	}
+	
+	void FixedUpdate () {
+        if (isFlipping)
+        {
+            flipping();
+        }
+        else {
+            main();
+        }
+	}
+
+    public void flip() {
+        if (isFlipping) return;
+        isFlipping = true;
+        flipTime = Time.time;
+        saveVelocity = rigidbody.velocity;
+        rigidbody.isKinematic = true;
+    }
+
+    protected void flipping()
+    {
+        Vector3 scale = transform.localScale;
+        if (Time.time - flipTime < flipPeriod)
+        {
+            if (face)
+            {
+                scale.x = Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI);
+            }
+            else
+            {
+                scale.x = -Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI);
+            }
+            if (Time.time - flipTime > flipPeriod / 2)
+            {
+                if (!face && GetComponent<SpriteRenderer>().sprite == backSprite)
+                {
+                    GetComponent<SpriteRenderer>().sprite = frontSprite;
+                }
+                if (face && GetComponent<SpriteRenderer>().sprite == frontSprite)
+                {
+                    GetComponent<SpriteRenderer>().sprite = backSprite;
+                }
+            }
+        }
+        else
+        {
+            face = !face;
+            isFlipping = false;
+            rigidbody.isKinematic = false;
+            rigidbody.velocity = saveVelocity;
+        }
+
+        transform.localScale = scale; 
+    }
+
+    protected virtual void main() { 
+        //TODO: add main
+    };
+
+}
+
