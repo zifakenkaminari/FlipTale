0e2751640d025295873046687c6a1c857dc3dc27
JoJorge <jorge13jorge13@gmail.com>
2016-09-01 16:08:46 +0800

    create stage and stage trigger

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+    void Update()
+    {
+    }
+	void FixedUpdate () {
+        Vector3 pos = transform.position;
+		pos.x = Mathf.Lerp(pos.x, player.GetComponent<Player>().nowStage.transform.position.x, 0.1f);
+        transform.position = pos;
+	}
+}
---
+++ Assets/Script/StageTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageTrigger : MonoBehaviour {
+
+	public GameObject stage0;
+	public GameObject stage1;
+	public float autoMovePath;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	//change stage
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log ("enter trigger");
+		Player player = collider.GetComponent<Player> ();
+		if (player) {
+			if (player.nowStage == stage0)
+				player.nowStage = stage1;
+			else
+				player.nowStage = stage0;
+			Vector3 pos = player.transform.position;
+			pos.x += ((player.nowStage.transform.position.x > pos.x)?1:-1) * autoMovePath;
+			player.transform.position = pos;
+		}
+	}
+}
