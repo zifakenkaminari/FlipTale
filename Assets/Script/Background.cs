f3ffa4b51a8004ac4f5e0a223bd205b12053907c
JoJorge <jorge13jorge13@gmail.com>
2016-09-01 16:52:52 +0800

    cos change background and create stage boundary

--- Assets/Script/Background.cs
+++ Assets/Script/Background.cs
@@ -39,22 +39,22 @@ public class Background : MonoBehaviour {
 			if (face)
 			{
 				// -- change background slow to fast --
-				//setTransparent(ref bgFront, Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
-				//setTransparent(ref bgBack, 1 - Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
+				setTransparent(ref bgFront, Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
+				setTransparent(ref bgBack, 1 - Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
 
 				// -- uniformly change background --
-				setTransparent(ref bgFront, 1 - (Time.time - flipTime) / flipPeriod);
-				setTransparent(ref bgBack, (Time.time - flipTime) / flipPeriod);
+				//setTransparent(ref bgFront, 1 - (Time.time - flipTime) / flipPeriod);
+				//setTransparent(ref bgBack, (Time.time - flipTime) / flipPeriod);
 			}
 			else
 			{
 				// -- change background slow to fast --
-				//setTransparent(ref bgFront, 1 - Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
-				//setTransparent(ref bgBack, Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
+				setTransparent(ref bgFront, 1 - Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
+				setTransparent(ref bgBack, Mathf.Cos((Time.time - flipTime) / flipPeriod * Mathf.PI / 2) );
 
 				// -- uniformly change background --
-				setTransparent(ref bgFront, (Time.time - flipTime) / flipPeriod);
-				setTransparent(ref bgBack, 1 - (Time.time - flipTime) / flipPeriod);
+				//setTransparent(ref bgFront, (Time.time - flipTime) / flipPeriod);
+				//setTransparent(ref bgBack, 1 - (Time.time - flipTime) / flipPeriod);
 			}
 		}
 		else
