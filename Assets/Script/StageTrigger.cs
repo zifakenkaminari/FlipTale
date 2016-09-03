1b9334854ed3463fb1fbd65f9555d453afbea02e
JoJorge <jorge13jorge13@gmail.com>
2016-09-03 21:15:51 +0800

    change flipper to stage#_#, get flipables from children, and fix player and item flip bugs

--- Assets/Script/StageTrigger.cs
+++ Assets/Script/StageTrigger.cs
@@ -15,10 +15,14 @@ public class StageTrigger : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D collider) {
 		Player player = collider.GetComponent<Player> ();
 		if (player) {
-			if (player.nowStage == stage0)
+			if (player.nowStage == stage0) {
 				player.nowStage = stage1;
-			else
+				player.transform.parent = stage1.transform;
+			}
+			else {
 				player.nowStage = stage0;
+				player.transform.parent = stage0.transform;
+			}
 			Vector3 pos = player.transform.position;
 			pos.x += ((player.nowStage.transform.position.x > pos.x)?1:-1) * autoMovePath;
 			player.transform.position = pos;
