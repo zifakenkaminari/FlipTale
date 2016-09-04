7a4ad5f66a6e9742b55ed4591a2d72458b2c347c
Sean <b03902126@csie.ntu.edu.tw>
2016-09-05 00:04:27 +0900

    Copy items from old project.

---
+++ Assets/Script/StoneSeedPile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneSeedPile : Item {
+    public GameObject stoneSeed;
+
+    public override void pick(GameObject player) {
+        GameObject newSeed = (GameObject)Instantiate(stoneSeed);
+        newSeed.pick(player);
+        player.GetComponent<Player>().itemOnHand = newSeed;
+    }
+
+}
