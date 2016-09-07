d355a995696e3fdb34639fae58d46d51131ed5d0
Sean <b03902126@csie.ntu.edu.tw>
2016-09-07 17:37:03 +0900

    Incredible bug.

---
+++ Assets/Script/TrashBag.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashBag : Item {
+
+	// Use this for initialization
+	protected override void Start () {
+        base.Start();
+        //pickable = false;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/WolfStatue.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfStatue : Entity {
+
+
+    protected override void Start() {
+        base.Start();
+    }
+
+
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("TrashBag"))
+        {
+            collider.gameObject.GetComponent<TrashBag>().pickable = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("TrashBag"))
+        {
+            collider.gameObject.GetComponent<TrashBag>().pickable = true;
+        }
+    }
+}
