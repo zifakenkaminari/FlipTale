1bdc4f95975a906c3bb6e377598347a80998b133
Sean <b03902126@csie.ntu.edu.tw>
2016-09-08 23:21:22 +0900

    Start to make ladder.

---
+++ Assets/Prefab/BoxLadder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxLadder : Item {
+
+    protected override void use(GameObject player)
+    {
+
+        Collider2D[] hits = overlapAreaAll();
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.gameObject.GetComponent<Ladder>())
+            {
+                hit.gameObject.GetComponent<Ladder>().build();
+                Destroy(gameObject);
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
