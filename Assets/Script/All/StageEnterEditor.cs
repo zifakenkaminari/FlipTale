ff3253acce66509269f115c9ed91cd50243778ff
JoJorge <jorge13jorge13@gmail.com>
2016-11-30 13:40:10 +0800

    make StageEnter better

---
+++ Assets/Script/All/StageEnterEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(StageEnter))]
+public class StageEnterEditor : Editor {
+
+    public override void OnInspectorGUI () {
+        DrawDefaultInspector ();
+        StageEnter myTarget = (StageEnter)target;        
+        EditorGUILayout.LabelField ("Can Enter");
+        EditorGUI.indentLevel++;
+        myTarget.canEnter [0] = EditorGUILayout.Toggle ("Front", myTarget.canEnter[0]);
+        myTarget.canEnter[1] = EditorGUILayout.Toggle ("Back", myTarget.canEnter [1]);
+    }
+
+}
